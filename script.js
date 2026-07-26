2ea4e4d9b3794aa6d2b2b06b113b247f38031f43
chandarajeshkumar10-maker <chandarajeshkumar10@gmail.com>
2026-07-26 22:08:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,61 +1,275 @@
-//alert("JavaScript Loaded");
-// Lightbox
-var lightbox = document.getElementById("lightbox");
-var lightboxImg = document.getElementById("lightbox-img");
-var closeBtn = document.querySelector(".close");
-
-document.querySelectorAll(".gallery img").forEach(function(img){
-    img.onclick = function(){
-        lightbox.style.display = "block";
-        lightboxImg.src = this.src;
-    };
-});
-
-closeBtn.onclick = function(){
-    lightbox.style.display = "none";
-};
-
-lightbox.onclick = function(e){
-    if(e.target == lightbox){
-        lightbox.style.display = "none";
-    }
-};
+/* ================================= */
+/* PRODUCT SEARCH */
+/* ================================= */
 
 function searchProduct() {
 
-    var input = document.getElementById("searchBox").value.toUpperCase();
+    var input = document.getElementById("searchBox");
 
-    var cards = document.getElementsByClassName("card");
-
-    for (var i = 0; i < cards.length; i++) {
-
-        var code = cards[i].getElementsByTagName("h3")[0].innerHTML.toUpperCase();
-
-        if (code.indexOf(input) !== -1 || input === "") {
-            cards[i].style.display = "inline-block";
-        } else {
-            cards[i].style.display = "none";
-        }
-
+    if (!input) {
+        return;
     }
-}
 
-function filterProducts(category) {
+    var searchValue = input.value.toLowerCase();
 
-    const products = document.querySelectorAll(".card");
+    var cards = document.querySelectorAll(".gallery .card");
 
-    products.forEach(function(product) {
+    cards.forEach(function(card) {
 
-        if (category === "all") {
-            product.style.display = "";
-        } 
-        else if (product.dataset.category === category) {
-            product.style.display = "";
-        } 
-        else {
-            product.style.display = "none";
+        var cardText = card.innerText.toLowerCase();
+
+        if (cardText.includes(searchValue)) {
+
+            card.style.display = "inline-block";
+
+        } else {
+
+            card.style.display = "none";
+
         }
 
     });
 
 }
+
+
+/* ================================= */
+/* CATEGORY FILTER */
+/* ================================= */
+
+function filterProducts(category) {
+
+    var cards = document.querySelectorAll(".gallery .card");
+
+    cards.forEach(function(card) {
+
+        var cardCategory =
+            card.getAttribute("data-category");
+
+        if (
+            category === "all" ||
+            cardCategory === category
+        ) {
+
+            card.style.display = "inline-block";
+
+        } else {
+
+            card.style.display = "none";
+
+        }
+
+    });
+
+}
+
+
+/* ================================= */
+/* FESTIVAL OFFERS */
+/* ================================= */
+
+function showFestivalOffer() {
+
+    var message =
+        document.getElementById("festivalMessage");
+
+    if (!message) {
+        return;
+    }
+
+    message.innerHTML =
+        "🎊 Festival Special Offer! 🎊<br>" +
+        "Get exciting discounts on selected products.<br>" +
+        "Order now on WhatsApp!";
+
+}
+
+
+/* ================================= */
+/* PRODUCT DETAILS POPUP */
+/* ================================= */
+
+function openProduct(
+    code,
+    name,
+    price,
+    image
+) {
+
+    var popup =
+        document.getElementById("productPopup");
+
+    var popupImage =
+        document.getElementById("popupImage");
+
+    var popupCode =
+        document.getElementById("popupCode");
+
+    var popupName =
+        document.getElementById("popupName");
+
+    var popupPrice =
+        document.getElementById("popupPrice");
+
+
+    /* PRODUCT IMAGE */
+
+    popupImage.src = image;
+
+    popupImage.alt = name;
+
+
+    /* PRODUCT DETAILS */
+
+    popupCode.innerText =
+        "Code: " + code;
+
+    popupName.innerText =
+        name;
+
+    popupPrice.innerText =
+        price;
+
+
+    /* SHOW POPUP */
+
+    popup.style.display = "block";
+
+}
+
+
+/* ================================= */
+/* CLOSE PRODUCT POPUP */
+/* ================================= */
+
+function closeProduct() {
+
+    var popup =
+        document.getElementById("productPopup");
+
+    popup.style.display = "none";
+
+}
+
+
+/* ================================= */
+/* CLOSE POPUP WHEN CLICKING OUTSIDE */
+/* ================================= */
+
+window.addEventListener(
+    "click",
+    function(event) {
+
+        var popup =
+            document.getElementById("productPopup");
+
+        if (
+            event.target === popup
+        ) {
+
+            popup.style.display =
+                "none";
+
+        }
+
+    }
+);
+
+
+/* ================================= */
+/* IMAGE LIGHTBOX */
+/* ================================= */
+
+document.addEventListener(
+    "DOMContentLoaded",
+    function() {
+
+        var lightbox =
+            document.getElementById("lightbox");
+
+        var lightboxImg =
+            document.getElementById("lightbox-img");
+
+        var closeButton =
+            document.querySelector(".close");
+
+
+        /* CHECK LIGHTBOX EXISTS */
+
+        if (
+            !lightbox ||
+            !lightboxImg
+        ) {
+
+            return;
+
+        }
+
+
+        /* ALL GALLERY IMAGES */
+
+        var galleryImages =
+            document.querySelectorAll(
+                ".gallery img"
+            );
+
+
+        /* OPEN IMAGE */
+
+        galleryImages.forEach(
+            function(img) {
+
+                img.addEventListener(
+                    "click",
+                    function() {
+
+                        lightbox.style.display =
+                            "block";
+
+                        lightboxImg.src =
+                            img.src;
+
+                    }
+                );
+
+            }
+        );
+
+
+        /* CLOSE BUTTON */
+
+        if (closeButton) {
+
+            closeButton.addEventListener(
+                "click",
+                function() {
+
+                    lightbox.style.display =
+                        "none";
+
+                }
+            );
+
+        }
+
+
+        /* CLICK OUTSIDE IMAGE */
+
+        lightbox.addEventListener(
+            "click",
+            function(event) {
+
+                if (
+                    event.target === lightbox
+                ) {
+
+                    lightbox.style.display =
+                        "none";
+
+                }
+
+            }
+        );
+
+    }
+);
